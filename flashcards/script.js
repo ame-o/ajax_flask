05593f95263deaa1464060777a9b47b3ec405630
America Ocampo <americavocampo@gmail.com>
2022-03-30 23:28:03 -0700

    mid-project commit

--- flashcards/script.js
+++ flashcards/script.js
@@ -6,11 +6,15 @@ card.addEventListener('click', function () {
 });
 
 async function call_word() {
-    var response = await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/adjacent");
+    var spelling = document.getElementById("spelling")
+    console.log(spelling.innerText);
+    var response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${spelling.innerText}`);
     var word = await response.json();
     console.log(word);
     link = word[0].phonetics[0].audio
     pronounciation = document.getElementById("pronounciation")
     pronounciation.src = link
+    // return link
 }
+call_word();
 
--- v/project/server.py
+++
@@ -1,8 +0,0 @@
-from flask_app import app
-#controllers
-from flask_app.controllers import user_controller, vocab_controller, essay_controller
-
-
-
-if __name__ == "__main__":
-    app.run(debug = True)
